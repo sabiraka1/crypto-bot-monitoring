14e4812919cbf46424619b05439490d46b5b0348
satis <sabiraka1@gmail.com>
2025-09-02 19:45:48 +0300

    0

---
+++ alertmanager/entrypoint.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/sh
+# Заменяем переменные в конфиге
+sed -i "s|TELEGRAM_BOT_TOKEN_PLACEHOLDER|${TELEGRAM_BOT_TOKEN}|g" /etc/alertmanager/alertmanager.yml
+sed -i "s|TELEGRAM_CHAT_ID_PLACEHOLDER|${TELEGRAM_CHAT_ID}|g" /etc/alertmanager/alertmanager.yml
+
+# Запускаем Alertmanager
+exec /bin/alertmanager --config.file=/etc/alertmanager/alertmanager.yml --storage.path=/alertmanager
